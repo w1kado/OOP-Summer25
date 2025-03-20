f36bd1af6412ef0ab4d5cdd39ef2c35d567101a6
w1kado <tunakasap2812@gmail.com>
2025-03-20 13:25:32 +0000

    Add new fileanimal.py

---
+++ animal.py
@@ -0,0 +1,55 @@
+dictiger = {
+"Name" : "Tiger",
+"Group" : "Feline",
+"Number of Legs" : 4,
+"Skills" : "Running fast , Swimming , Strong Chin , Strong claw"
+}
+dictsnake ={
+"Name" : "Snake",
+"Group" : "Reptile",
+"Number of Legs" : 0,
+"Skills" : "Make rapid progress , Strong bite , Strog claw"
+}
+dictfrog = {
+"Name" : "Frog",
+"Group" : "Amphibians",
+"Number of Legs" : 4,
+"Skills" : "Jumping high , Swimming , Deadful Posion"
+}
+dictboar = {
+"Name" : "Wild Boar",
+"Group" : "Suidae",
+"Number of Legs" : 4,
+"Skills" : "80 KM per hour , Long and sharp teeth , Strong sense of smell"
+}
+dictbear = {
+"Name" : "Polar Bear",
+"Group" : "Ursidae",
+"Number of Legs" : 4,
+"Skills" : "Wonderful speaking with humans , Fishing , Destroy beehive"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(dictbear)
+print(dictboar)
+print(dictfrog)
+print(dictiger)
+print(dictsnake)
+print("This project Made by Tuna Basar KASAP")
